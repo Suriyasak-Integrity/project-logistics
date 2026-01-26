388ca4b9f51ea48bca36560fcccdd2033eabe8b2
Suriyasak Nutchanart <193051942+Suriyasak-Integrity@users.noreply.github.com>
2026-01-26 17:39:45 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,9 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { ThemeProvider } from '@/components/ThemeProvider';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  subsets: ['latin']
+});
 
 export const metadata = {
   title: 'Integrity Logistics Thailand - Beyond the Average Freight Forwarder',
