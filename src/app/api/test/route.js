d8a23ae8ea3c49edf460489e2277b9db5b3a7e3b
Suriyasak Nutchanart <193051942+Suriyasak-Integrity@users.noreply.github.com>
2026-01-27 11:29:53 +0700

    Update route.js

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,9 +1,11 @@
+export const dynamic = "force-dynamic";
+
 export async function GET() {
-    return new Response(
-        JSON.stringify({
-            chatbotId: process.env.NEXT_PUBLIC_CHATBOT_ID,
-            hasKernelKey: !!process.env.KERNEL_API_KEY,
-        }),
-        { headers: { "Content-Type": "application/json" } }
-    );
+  return new Response(
+    JSON.stringify({
+      chatbotId: process.env.NEXT_PUBLIC_CHATBOT_ID,
+      hasKernelKey: !!process.env.KERNEL_API_KEY,
+    }),
+    { headers: { "Content-Type": "application/json" } }
+  );
 }
